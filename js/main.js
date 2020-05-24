c54fbcb729c0ea62129f4126c07928b2b88dde9e
Ivan Tsimbalist <iv-tsim@yandex.ru>
2020-05-24 18:39:08 +0300

    complete goods section

--- js/main.js
+++ js/main.js
@@ -9,4 +9,21 @@ $(document).ready(function(){
     nextArrow: '<div class="hero-slider__arrow hero-slider__arrow_right"></div>',
     appendDots: $('.hero-slider__dots')
   });
+  const goodsWrap = document.querySelector('.goods');
+  const images = (event) => {
+    const {target} = event;
+
+    if(target.matches('a.goods-items__block-text')) {
+        const goodsPhoto = target.closest('.goods-items__block').children[0];
+        const source = goodsPhoto.getAttribute('src');
+        goodsPhoto.src = goodsPhoto.dataset.img;
+        goodsPhoto.dataset.img = source;
+    }
+  };
+  goodsWrap.addEventListener('mouseenter', (event) => {
+    images(event);
+  }, true);
+  goodsWrap.addEventListener('mouseleave', (event) => {
+    images(event);
+  }, true);
 });
